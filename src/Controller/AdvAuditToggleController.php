ad72624947215ff3422112208779c899ba7f786e
Elena Tolkacheva <etolkacheva@adyax.com>
2018-05-23 10:56:48 +0300

    Task ref #305778: Fix routing.

--- src/Controller/AdvAuditToggleController.php
+++ src/Controller/AdvAuditToggleController.php
@@ -2,67 +2,18 @@
 
 namespace Drupal\adv_audit\Controller;
 
-use Drupal\Core\Access\CsrfTokenGenerator;
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Responsible for handling the toggle links on the Run & Review page.
  */
-class DrupalAuditorToggleController extends ControllerBase {
+class AdvAuditToggleController extends ControllerBase {
 
   /**
-   * The CSRF Token generator.
-   *
-   * @var \Drupal\Core\Access\CsrfTokenGenerator $csrfToken
+   * Creates the Run & Review page.
    */
-  protected $csrfToken;
-
-  /**
-   * The request stack.
-   *
-   * @var \Symfony\Component\HttpFoundation\Request $request
-   */
-  protected $request;
-
-  /**
-   * Constructs a ToggleController.
-   *
-   * @param \Drupal\Core\Access\CsrfTokenGenerator $csrf_token_generator
-   *   The CSRF Token generator.
-   * @param \Symfony\Component\HttpFoundation\RequestStack $request
-   *   The request stack.
-   */
-  public function __construct(CsrfTokenGenerator $csrf_token_generator, RequestStack $request) {
-    $this->csrfToken = $csrf_token_generator;
-    $this->request = $request->getCurrentRequest();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('csrf_token'),
-      $container->get('request_stack')
-    );
-  }
-
-  /**
-   * Handles check toggling.
-   *
-   * @param string $check_id
-   *   The ID of the check.
-   *
-   * @return \Symfony\Component\HttpFoundation\JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
-   *   The response.
-   */
-  public function index($check_id) {
-
-
-    // Go back to Run & Review if the access was wrong.
-    return $this->redirect('adv_audit');
+  public function index() {
+    return 'Dummy page';
   }
 
 }
