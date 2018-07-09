39be4bdd1bac173d30745482944a86fa8152556c
Elena Tolkacheva <etolkacheva@adyax.com>
2018-07-09 18:05:25 +0300

    Fix symfony compabilites

--- src/Plugin/AdvAuditCheckpoint/FeaturesStatus.php
+++ src/Plugin/AdvAuditCheckpoint/FeaturesStatus.php
@@ -6,7 +6,7 @@ use Drupal\adv_audit\Plugin\AdvAuditCheckpointBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\features\Package;
 use Drupal\features\FeaturesManagerInterface;
-use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Check non-security updates for contrib modules.
