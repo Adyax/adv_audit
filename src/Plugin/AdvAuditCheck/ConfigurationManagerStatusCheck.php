35ee0277bb1dfd59c8920c0ee48d42e3954e6650
YDrapoy <ydrapoy@adyax.com>
2018-08-07 20:50:43 +0300

    add ConfigurationManagerStatusCheck plugin

--- src/Plugin/AdvAuditCheck/ConfigurationManagerStatusCheck.php
+++ src/Plugin/AdvAuditCheck/ConfigurationManagerStatusCheck.php
@@ -2,16 +2,18 @@
 
 namespace Drupal\adv_audit\Plugin\AdvAuditCheck;
 
+use Drupal\adv_audit\AuditReason;
+use Drupal\adv_audit\AuditResultResponseInterface;
 use Drupal\adv_audit\Plugin\AdvAuditCheckBase;
 use Drupal\adv_audit\Plugin\AdvAuditCheckInterface;
 use Drupal\Core\Config\CachedStorage;
 use Drupal\Core\Config\ConfigManager;
 use Drupal\Core\Config\DatabaseStorage;
+use Drupal\Core\Config\StorageComparer;
+use Drupal\Core\Config\StorageInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\file\FileStorage;
 use Drupal\update\UpdateManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
 
 /**
  * @AdvAuditCheck(
@@ -39,14 +41,14 @@ class ConfigurationManagerStatusCheck extends AdvAuditCheckBase implements AdvAu
    * Constructs Configuration Manager Status.
    */
   public function __construct(
-    array $configuration,
-    string $plugin_id,
-    mixed $plugin_definition,
+    $configuration,
+    $plugin_id,
+    $plugin_definition,
     UpdateManager $update_manager,
     ConfigManager $config_manager,
     CachedStorage $config_storage,
     DatabaseStorage $config_storage_snapshot,
-    FileStorage $config_storage_sync
+    StorageInterface $config_storage_sync
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->updateManager = $update_manager;
@@ -78,22 +80,17 @@ class ConfigurationManagerStatusCheck extends AdvAuditCheckBase implements AdvAu
   public function perform() {
     $storage_comparer = new StorageComparer($this->syncStorage, $this->activeStorage, $this->configManager);
     $is_overriden = $storage_comparer->createChangelist()->hasChanges();
-    if ($is_overriden) {
-      $this->setProcessStatus('fail');
-    }
-    // Collect check results.
-    $result = [
-      'title' => $this->getTitle(),
-      'description' => $this->getDescription(),
-      'information' => $this->getProcessResult(),
-      'status' => $this->getProcessStatus(),
-      'severity' => $this->get('severity'),
-      'actions' => $this->getActions(),
-      'impacts' => $this->getImpacts(),
-    ];
 
-    $results[$this->get('category')][$this->getPluginId()] = $result;
-    return $results;
+    if ($is_overriden) {
+      return new AuditReason(
+        $this->id(),
+        AuditResultResponseInterface::RESULT_FAIL,
+        $this->t('There are differences between configurations stored in database and files.')
+      );
+    }
+    else {
+      return new AuditReason($this->id(), AuditResultResponseInterface::RESULT_PASS);
+    }
   }
 
 }
