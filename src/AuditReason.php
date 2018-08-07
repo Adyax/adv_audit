1f49720ba6c308245c577d620167830ae578c4b3
YDrapoy <ydrapoy@adyax.com>
2018-08-07 11:18:50 +0300

    Task #318367: AuditReason - Add a new line at the end of this file + pre-commit

--- src/AuditReason.php
+++ src/AuditReason.php
@@ -4,6 +4,7 @@ namespace Drupal\adv_audit;
 
 /**
  * Class AuditReason.
+ *
  * Use in audit test plugin for save perform result.
  *
  * @package Drupal\adv_audit
@@ -11,23 +12,30 @@ namespace Drupal\adv_audit;
 class AuditReason {
 
   protected $status;
-  protected $test_id;
-  protected $message_type;
+  protected $testId;
+  protected $messageType;
 
-
+  /**
+   * Constructs AuditReason.
+   */
   public function __construct($plugin_id, $status, $msg_type) {
     $this->status = $status;
-    $this->test_id = $plugin_id;
-    $this->message_type = $msg_type;
+    $this->testId = $plugin_id;
+    $this->messageType = $msg_type;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public static function create($plugin_id, $status, $msg_type) {
     return new static($plugin_id, $status, $msg_type);
   }
 
+  /**
+   * Get Status of audit.
+   */
   public function getStatus() {
     return $this->status;
   }
-
 
 }
