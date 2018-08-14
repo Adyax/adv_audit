74a70c702a29f11ad7c5c78797833218460c83ec
Alex <ashipilov@adyax.com>
2018-08-14 19:24:35 +0300

    #304953 - register globals plugin

---
+++ src/Plugin/AdvAuditCheck/RegisterGlobalsCheck.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\adv_audit\Plugin\AdvAuditCheck;
+
+use Drupal\adv_audit\Plugin\AdvAuditCheckBase;
+use Drupal\adv_audit\AuditReason;
+use Drupal\adv_audit\AuditResultResponseInterface;
+
+/**
+ * Register Globals Check plugin class.
+ *
+ * @AdvAuditCheck(
+ *   id = "register_globals_check",
+ *   label = @Translation("PHP register globals"),
+ *   category = "security",
+ *   requirements = {},
+ *   enabled = true,
+ *   severity = "high"
+ * )
+ */
+class RegisterGlobalsCheck extends AdvAuditCheckBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function perform() {
+    $register_globals = trim(ini_get('register_globals'));
+    if (!empty($register_globals) && strtolower($register_globals) != 'off') {
+      $status = AuditResultResponseInterface::RESULT_FAIL;
+    }
+
+    return new AuditReason($this->id(), $status, NULL, []);
+  }
+
+}
