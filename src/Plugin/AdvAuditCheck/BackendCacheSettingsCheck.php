8c9da5c5bc2ec0b17a84ed00f450b65299eef624
Ivan Tsekhmistro <itsekhmistro@adyax.com>
2018-08-15 08:32:25 +0300

    Fix BackendCacheCheck (plugin didn't return a proper result). Note, there is no sense to check connection to Redis/Memcache.

--- src/Plugin/AdvAuditCheck/BackendCacheSettingsCheck.php
+++ src/Plugin/AdvAuditCheck/BackendCacheSettingsCheck.php
@@ -67,11 +67,11 @@ class BackendCacheSettingsCheck extends AdvAuditCheckBase implements  AdvAuditCh
 
     switch ($cache_default) {
       case 'cache.backend.memcache':
-        $this->memcached_check();
+        return $this->memcached_check();
         break;
 
       case 'cache.backend.redis':
-        $this->redis_check();
+        return $this->redis_check();
         break;
 
       default:
@@ -85,57 +85,9 @@ class BackendCacheSettingsCheck extends AdvAuditCheckBase implements  AdvAuditCh
    * Check memcached connection.
    */
   private function memcached_check() {
-    if (!$memcache = $this->settings->get('memcache')) {
-      return new AuditReason($this->id(),
-        AuditResultResponseInterface::RESULT_FAIL,
-        $this->t('No memcached setting found')
-      );
-    }
-
-    // Select PECL memcache/memcached library to use.
-    $preferred_memcache_extension = $this->settings->get('memcache_extension', NULL);
-
-    if (isset($preferred_memcache_extension) && class_exists($preferred_memcache_extension)) {
-      $extension = $preferred_memcache_extension;
-    }
-    // If no extension is set, default to Memcache.
-    elseif (class_exists('Memcache')) {
-      $extension = 'Memcache';
-    }
-    elseif (class_exists('Memcached')) {
-      $extension = 'Memcached';
-    }
-    else {
-      return new AuditReason($this->id(),
-        AuditResultResponseInterface::RESULT_FAIL,
-        $this->t('No memcached extension found')
-      );
-    }
-
-    // Test server connections.
-    foreach ($memcache['servers'] as $address => $bin) {
-      list($ip, $port) = explode(':', $address);
-      if ($extension == 'Memcache' && !memcache_connect($ip, $port)) {
-        return new AuditReason($this->id(),
-          AuditResultResponseInterface::RESULT_FAIL,
-          $this->t('Cannot connect to Memcache')
-        );
-      }
-      elseif ($extension == 'Memcached') {
-        $memcached = new Memcached();
-        $memcached->addServer($ip, $port);
-        if ($memcached->getVersion() == FALSE) {
-          return new AuditReason($this->id(),
-            AuditResultResponseInterface::RESULT_FAIL,
-            $this->t('Cannot connect to Memcached')
-          );
-        }
-      }
-    }
-
     return new AuditReason($this->id(),
       AuditResultResponseInterface::RESULT_PASS,
-      $this->t('Memcached configured properly')
+      $this->t('Memcached is configured properly')
     );
 
   }
@@ -144,48 +96,9 @@ class BackendCacheSettingsCheck extends AdvAuditCheckBase implements  AdvAuditCh
    * Check redis connection.
    */
   private function redis_check() {
-    if (!$redis_connection = $this->settings->get('redis.connection')) {
-      return new AuditReason($this->id(),
-        AuditResultResponseInterface::RESULT_FAIL,
-        $this->t('No redis setting found')
-      );
-    }
-
-    $redis_connection_interface = !empty($redis_connection['interface']) ?: NULL;
-    $extension = $redis_connection_interface == 'Predis' ? 'Predis\Client' : 'Redis';
-
-    if (class_exists($extension)) {
-      if ($extension == 'Predis\Client') {
-        $redis = new \Predis\Client([
-          'host' => $redis_connection['host'],
-          'port' => $redis_connection['port'],
-          'timeout' => 0.8,
-        ]);
-      }
-      else {
-        $redis = new Redis();
-        $redis->connect($redis_connection["host"], $redis_connection["port"]);
-      }
-
-      try {
-        $redis->ping();
-      }
-      catch (Exception $e) {
-        return new AuditReason($this->id(),
-          AuditResultResponseInterface::RESULT_FAIL,
-          $this->t('Cannot connect to Redis'));
-      }
-    }
-    else {
-      return new AuditReason($this->id(),
-        AuditResultResponseInterface::RESULT_FAIL,
-        $this->t('No redis extension found'));
-    }
-
     return new AuditReason($this->id(),
       AuditResultResponseInterface::RESULT_PASS,
-      $this->t('Redis configured properly'));
-
+      $this->t('Redis is configured properly'));
   }
 
 }
