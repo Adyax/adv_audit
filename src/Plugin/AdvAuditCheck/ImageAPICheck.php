0e9af4ffd9d09577d9c0192a80c0b6cafefbbe66
Elena Tolkacheva <etolkacheva@adyax.com>
2018-08-16 11:23:42 +0300

    Task ref #304939: ImageAPI Optimize.

---
+++ src/Plugin/AdvAuditCheck/ImageAPICheck.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Drupal\adv_audit\Plugin\AdvAuditCheck;
+
+use Drupal\adv_audit\AuditReason;
+use Drupal\adv_audit\AuditResultResponseInterface;
+use Drupal\adv_audit\Message\AuditMessagesStorageInterface;
+use Drupal\adv_audit\Plugin\AdvAuditCheckBase;
+use Drupal\Core\Link;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\adv_audit\Renderer\AdvAuditReasonRenderableInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\State\StateInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Url;
+
+/**
+ * Provide imageApi optimize check.
+ *
+ * @AdvAuditCheck(
+ *  id = "imageapi_optimize_check",
+ *  label = @Translation("ImageAPI Optimize"),
+ *  category = "performance",
+ *  severity = "low",
+ *  enabled = true,
+ *  requirements = {},
+ * )
+ */
+class ImageAPICheck extends AdvAuditCheckBase implements ContainerFactoryPluginInterface, AdvAuditReasonRenderableInterface {
+
+  /**
+   * Drupal\Core\State\StateInterface definition.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * The audit messages storage service.
+   *
+   * @var \Drupal\adv_audit\Message\AuditMessagesStorageInterface
+   */
+  protected $messagesStorage;
+
+  /**
+   * Interface for working with drupal module system.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * Constructs a new ExampleAuditCheckPlugin object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\State\StateInterface $state
+   *   Interface for the state system.
+   * @param \Drupal\adv_audit\Message\AuditMessagesStorageInterface $messages_storage
+   *   Interface for the audit messages.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   Interface for working with drupal module system.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StateInterface $state, AuditMessagesStorageInterface $messages_storage, ModuleHandlerInterface $module_handler) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->state = $state;
+    $this->messagesStorage = $messages_storage;
+    $this->moduleHandler = $module_handler;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('state'),
+      $container->get('adv_audit.messages'),
+      $container->get('module_handler')
+    );
+  }
+
+  /**
+   * The actual procedure of carrying out the check.
+   *
+   * @return \Drupal\adv_audit\AuditReason
+   *   Return AuditReason object instance.
+   */
+  public function perform() {
+    // Created placeholder link for messages.
+    $url = Url::fromUri('https://www.drupal.org/project/imageapi_optimize', ['attributes' => ['target' => '_blank']]);
+    $link = Link::fromTextAndUrl('ImageAPI Optimize', $url);
+    if (!$this->moduleHandler->moduleExists('imageapi_optimize')) {
+      return new AuditReason($this->id(), AuditResultResponseInterface::RESULT_FAIL, NULL, ['%link' => $link->toString()]);
+    }
+    return new AuditReason($this->id(), AuditResultResponseInterface::RESULT_PASS, NULL, ['%link' => $link->toString()]);
+  }
+}
