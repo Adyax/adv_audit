63a97ed913916c7ce7bb691950c77564b805d63d
YDrapoy <ydrapoy@adyax.com>
2018-08-21 11:44:03 +0300

    use dependency injection

--- src/Plugin/AdvAuditCheck/AdminUserNameCheck.php
+++ src/Plugin/AdvAuditCheck/AdminUserNameCheck.php
@@ -5,6 +5,11 @@ namespace Drupal\adv_audit\Plugin\AdvAuditCheck;
 use Drupal\adv_audit\AuditReason;
 use Drupal\adv_audit\AuditResultResponseInterface;
 use Drupal\adv_audit\Plugin\AdvAuditCheckBase;
+use Drupal\adv_audit\Renderer\AdvAuditReasonRenderableInterface;
+
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin for check Administrator's name.
@@ -18,7 +23,34 @@ use Drupal\adv_audit\Plugin\AdvAuditCheckBase;
  *   enabled = TRUE,
  * )
  */
-class AdminUserNameCheck extends AdvAuditCheckBase {
+class AdminUserNameCheck extends AdvAuditCheckBase implements ContainerFactoryPluginInterface, AdvAuditReasonRenderableInterface {
+
+  /**
+   * Entity type manager container.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManager $entity_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -27,7 +59,7 @@ class AdminUserNameCheck extends AdvAuditCheckBase {
     global $base_url;
 
     // Get admin's name.
-    $user = \Drupal::entityTypeManager()->getStorage('user')->load(1);
+    $user = $this->entityTypeManager->getStorage('user')->load(1);
     $adminNAme = $user->get('name')->value;
     $secure = TRUE;
     $arguments = [];
@@ -37,20 +69,21 @@ class AdminUserNameCheck extends AdvAuditCheckBase {
     // Get host.
     $parsed_base = parse_url($base_url);
     $host_parts = explode('.', $parsed_base['host']);
-    $name_contains_host_part = FALSE;
+
     foreach ($host_parts as $part) {
       if (stripos($adminNAme, $part) !== FALSE) {
-        $name_contains_host_part = TRUE;
+        $secure = FALSE;
         $arguments['has_host_parts'][] = $part;
       }
     }
 
     // The username contains "admin".
-    if (stripos($adminNAme, 'admin') !== FALSE || $name_contains_host_part) {
+    if (stripos($adminNAme, 'admin') !== FALSE) {
       $secure = FALSE;
       $arguments['has_admin_parts'] = TRUE;
     }
 
+    // Very bad variant for administrator's name.
     if ($adminNAme == 'admin') {
       $arguments['has_default_admin_name'] = TRUE;
     }
@@ -59,6 +92,13 @@ class AdminUserNameCheck extends AdvAuditCheckBase {
       return new AuditReason($this->id(), AuditResultResponseInterface::RESULT_FAIL, NULL, $arguments);
     }
     return new AuditReason($this->id(), AuditResultResponseInterface::RESULT_PASS);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function auditReportRender(AuditReason $reason, $type) {
+    // Render status.
   }
 
 }
