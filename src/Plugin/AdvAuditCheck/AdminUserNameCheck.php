c56149551bb2d8e4812a47a5277774187b07dc5b
YDrapoy <ydrapoy@adyax.com>
2018-08-22 09:29:56 +0300

    fix in auditReportRender(), fix variable naming in perform()

--- src/Plugin/AdvAuditCheck/AdminUserNameCheck.php
+++ src/Plugin/AdvAuditCheck/AdminUserNameCheck.php
@@ -19,7 +19,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   id = "admin_name_check",
  *   label = @Translation("Administrator's name check"),
  *   category = "security",
- *   severity = "high",
+ *   severity = "normal",
  *   requirements = {},
  *   enabled = TRUE,
  * )
@@ -75,11 +75,11 @@ class AdminUserNameCheck extends AdvAuditCheckBase implements ContainerFactoryPl
 
     // Get admin's name.
     $user = $this->entityTypeManager->getStorage('user')->load(1);
-    $adminNAme = $user->get('name')->value;
+    $admin_name = $user->get('name')->value;
     $secure = TRUE;
 
     $arguments = [
-      self::NAME_PLACEHOLDER => $adminNAme,
+      self::NAME_PLACEHOLDER => $admin_name,
     ];
 
     // Get host.
@@ -87,20 +87,20 @@ class AdminUserNameCheck extends AdvAuditCheckBase implements ContainerFactoryPl
     $host_parts = explode('.', $parsed_base['host']);
 
     foreach ($host_parts as $part) {
-      if (stripos($adminNAme, $part) !== FALSE) {
+      if (stripos($admin_name, $part) !== FALSE) {
         $secure = FALSE;
         $arguments[self::HAS_HOST][] = $part;
       }
     }
 
     // The username contains "admin".
-    if (stripos($adminNAme, self::ADMIN_NAME) !== FALSE) {
+    if (stripos($admin_name, self::ADMIN_NAME) !== FALSE) {
       $secure = FALSE;
       $arguments['has_admin_parts'] = TRUE;
     }
 
     // Very bad variant for administrator's name.
-    if ($adminNAme == self::ADMIN_NAME) {
+    if ($admin_name == self::ADMIN_NAME) {
       $arguments['has_default_admin_name'] = TRUE;
     }
 
@@ -118,17 +118,18 @@ class AdminUserNameCheck extends AdvAuditCheckBase implements ContainerFactoryPl
 
     $arguments = $reason->getArguments();
 
-    if ($arguments[self::HAS_HOST]) {
-      $items[] = 'There are host parts in admin username: ' . implode(', ', $arguments[self::HAS_HOST]);
-    }
-    if ($arguments['has_default_admin_name']) {
-      $items[] = 'Admin\'s username seems like default username for administrator';
-    }
-    if ($arguments['has_admin_parts'] && $arguments[self::NAME_PLACEHOLDER] != self::ADMIN_NAME) {
-      $items[] = 'There are "admin" parts in username';
-    }
-
     if ($type == AuditMessagesStorageInterface::MSG_TYPE_FAIL) {
+
+      if ($arguments[self::HAS_HOST]) {
+        $items[] = 'There are host parts in admin username: ' . implode(', ', $arguments[self::HAS_HOST]);
+      }
+      if ($arguments['has_default_admin_name']) {
+        $items[] = 'Admin\'s username seems like default username for administrator';
+      }
+      if ($arguments['has_admin_parts'] && $arguments[self::NAME_PLACEHOLDER] != self::ADMIN_NAME) {
+        $items[] = 'There are "admin" parts in username';
+      }
+
       $build['admin_name_check'] = [
         '#theme' => 'item_list',
         '#title' => $this->t('Current name of admin is %name', ['%name' => $arguments[self::NAME_PLACEHOLDER]]),
