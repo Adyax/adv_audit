2f56133ec488b02cb62d6a3bc7e44098900ddc07
Elena Tolkacheva <etolkacheva@adyax.com>
2018-08-23 15:06:50 +0300

    Task ref #304947: PHP files in the Drupal files directory cannot be executed.

--- src/Plugin/AdvAuditCheck/ExecuteFilesCheck.php
+++ src/Plugin/AdvAuditCheck/ExecuteFilesCheck.php
@@ -5,8 +5,10 @@ namespace Drupal\adv_audit\Plugin\AdvAuditCheck;
 use Drupal\adv_audit\AuditReason;
 use Drupal\adv_audit\AuditResultResponseInterface;
 use Drupal\adv_audit\Plugin\AdvAuditCheckBase;
+use Drupal\Core\Link;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\StreamWrapper\PublicStream;
+use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use GuzzleHttp\Client;
 
@@ -15,7 +17,7 @@ use GuzzleHttp\Client;
  *
  * @AdvAuditCheck(
  *  id = "execute_files",
- *  label = @Translation("PHP files in the Drupal files directory cannot be executed."),
+ *  label = @Translation("PHP files in public directory cannot be executed."),
  *  category = "security",
  *  severity = "high",
  *  requirements = {},
@@ -66,6 +68,11 @@ class ExecuteFilesCheck extends AdvAuditCheckBase implements ContainerFactoryPlu
   public function perform() {
     global $base_url;
     $status = AuditResultResponseInterface::RESULT_PASS;
+    $url = Url::fromUri('https://www.drupal.org/node/615888', ['attributes' => ['target' => '_blank']]);
+    $arguments = [
+      '%risk_link' => Link::fromTextAndUrl($this->t('Drupal.org'), $url)
+        ->toString(),
+    ];
 
     // Set up test file data.
     $message = 'Advanced audit execute php test ' . date('Ymdhis');
@@ -81,14 +88,15 @@ class ExecuteFilesCheck extends AdvAuditCheckBase implements ContainerFactoryPlu
     // Try to access the test file.
     try {
       $response = $this->httpClient->get($base_url . '/' . $file_path);
-      if ($response->getStatusCode() == 200 && $response->getBody()->getContents() == $message) {
+      $responce_body = $response->getBody()->getContents();
+      if ($response->getStatusCode() == 200 && $message == $responce_body) {
         $status = AuditResultResponseInterface::RESULT_FAIL;
       }
     }
     catch (RequestException $e) {
       // Access was denied to the file.
     }
-    return new AuditReason($this->id(), $status, NULL);
+    return new AuditReason($this->id(), $status, NULL, $arguments);
   }
 
 }
