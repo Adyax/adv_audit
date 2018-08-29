76df7e101df23dc58bd7da3eb5f5cd4ae6b81c13
Alex <ashipilov@adyax.com>
2018-08-29 14:58:41 +0300

    #304951 - refactoring

--- src/Plugin/AdvAuditCheck/UnsafeExtensionsCheck.php
+++ src/Plugin/AdvAuditCheck/UnsafeExtensionsCheck.php
@@ -8,6 +8,10 @@ use Drupal\adv_audit\Renderer\AdvAuditReasonRenderableInterface;
 use Drupal\adv_audit\Message\AuditMessagesStorageInterface;
 
 use Drupal\field\Entity\FieldConfig;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\State\StateInterface;
 
 /**
  * Unsafe extensions Check plugin class.
@@ -21,27 +25,51 @@ use Drupal\field\Entity\FieldConfig;
  *   severity = "high"
  * )
  */
-class UnsafeExtensionsCheck extends AdvAuditCheckBase implements AdvAuditReasonRenderableInterface {
+class UnsafeExtensionsCheck extends AdvAuditCheckBase implements AdvAuditReasonRenderableInterface, ContainerFactoryPluginInterface {
+  /**
+   * The state service object.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, StateInterface $state) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->state = $state;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('state')
+    );
+  }
+
+  /**
+   * Build key string for access to stored value from config.
+   *
+   * @return string
+   *   The generated key.
+   */
+  protected function buildStateConfigKey() {
+    return 'adv_audit.plugin.' . $this->id() . '.additional-settings';
+  }
 
   /**
    * {@inheritdoc}
    */
   public function perform() {
     $fields = [];
-    $unsafe_ext = [
-      'swf',
-      'exe',
-      'html',
-      'htm',
-      'php',
-      'phtml',
-      'py',
-      'js',
-      'vb',
-      'vbe',
-      'vbs',
-    ];
-
+    $settings = $this->getPerformSettings();
+    $unsafe_ext = explode(' ', $settings['unsafe_extensions']);
     // Check field configuration entities.
     foreach (FieldConfig::loadMultiple() as $entity) {
       $extensions = $entity->getSetting('file_extensions');
@@ -55,22 +83,7 @@ class UnsafeExtensionsCheck extends AdvAuditCheckBase implements AdvAuditReasonR
     }
 
     if (!empty($fields)) {
-      $items = [];
-      foreach ($fields as $entity_id => $unsafe_extensions) {
-        $entity = FieldConfig::load($entity_id);
-        foreach ($unsafe_extensions as $extension) {
-          $items[] = $this->t(
-            'Review @type in "@field" field on @bundle',
-            [
-              '@type' => $extension,
-              '@field' => $entity->label(),
-              '@bundle' => $entity->getTargetBundle(),
-            ]
-          )->render();
-        }
-      }
-
-      $params = ['fields' => $items];
+      $params = ['fields' => $fields];
       return $this->fail('Unsafe file extensions are allowed in uploads.', $params);
     }
 
@@ -80,15 +93,73 @@ class UnsafeExtensionsCheck extends AdvAuditCheckBase implements AdvAuditReasonR
   /**
    * {@inheritdoc}
    */
+  public function configForm() {
+    $settings = $this->getPerformSettings();
+    $form['unsafe_extensions'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Unsafe file extensions'),
+      '#default_value' => $settings['unsafe_extensions'],
+      '#description' => $this->t('List of unsafe file extensions, separated with coma without spaces.'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function configFormSubmit($form, FormStateInterface $form_state) {
+    $value = $form_state->getValue('unsafe_extensions');
+    $this->state->set($this->buildStateConfigKey(), $value);
+  }
+
+  /**
+   * Get settings for perform task.
+   */
+  protected function getPerformSettings() {
+    $settings = $this->state->get($this->buildStateConfigKey());
+    return !is_null($settings) ? $settings : $this->getDefaultPerformSettings();
+  }
+
+  /**
+   * Get default settings.
+   */
+  protected function getDefaultPerformSettings() {
+    return [
+      'unsafe_extensions' => 'swf,exe,html,htm,php,phtml,py,js,vb,vbe,vbs',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function auditReportRender(AuditReason $reason, $type) {
     if ($type == AuditMessagesStorageInterface::MSG_TYPE_FAIL) {
       $arguments = $reason->getArguments();
       if (!empty($arguments['fields'])) {
+        $items = [];
+        $fields = $arguments['fields'];
+        if (!empty($fields)) {
+          foreach ($fields as $entity_id => $unsafe_extensions) {
+            $entity = FieldConfig::load($entity_id);
+            foreach ($unsafe_extensions as $extension) {
+              $items[] = $this->t(
+                'Review @type in "@field" field on @bundle',
+                [
+                  '@type' => $extension,
+                  '@field' => $entity->label(),
+                  '@bundle' => $entity->getTargetBundle(),
+                ]
+              )->render();
+            }
+          }
+        }
+
         return [
           '#theme' => 'item_list',
           '#title' => $this->t('Fields with unsafe extensions:'),
           '#list_type' => 'ul',
-          '#items' => $arguments['fields'],
+          '#items' => $items,
         ];
       }
     }
