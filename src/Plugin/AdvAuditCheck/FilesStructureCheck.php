5ed2cc50fc40d7074c60279c0e84bfd0a916e034
YDrapoy <ydrapoy@adyax.com>
2018-09-06 16:33:45 +0300

    refactor filesystem structure check

--- src/Plugin/AdvAuditCheck/FilesStructureCheck.php
+++ src/Plugin/AdvAuditCheck/FilesStructureCheck.php
@@ -5,7 +5,6 @@ namespace Drupal\adv_audit\Plugin\AdvAuditCheck;
 use Drupal\adv_audit\Plugin\AdvAuditCheckBase;
 use Drupal\Core\DrupalKernel;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -29,12 +28,15 @@ class FilesStructureCheck extends AdvAuditCheckBase implements ContainerFactoryP
    */
   protected $kernel;
 
+  protected $issues;
+
   /**
    * {@inheritdoc}
    */
   public function __construct(array $configuration, $plugin_id, $plugin_definition, DrupalKernel $kernel) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->kernel = $kernel;
+    $this->issues = [];
   }
 
   /**
@@ -49,133 +51,82 @@ class FilesStructureCheck extends AdvAuditCheckBase implements ContainerFactoryP
     );
   }
 
-  const MODULES_BASE = DRUPAL_ROOT . '/modules';
+  const MODULES_BASE = 'modules';
 
-  const THEMES_BASE = DRUPAL_ROOT . '/themes';
+  const THEMES_BASE = 'themes';
 
   /**
    * {@inheritdoc}
    */
   public function perform() {
-    $issue_details = [];
-    $modules_in_base_failed = $this->scanFolder(self::MODULES_BASE);
-    $themes_in_base_failed = $this->scanFolder(self::THEMES_BASE);
-    $sites_folder_failed = $this->scanSitesFolder();
 
-    if (!empty($modules_in_base_failed)) {
-      $issue_details['modules_in_base'] = $modules_in_base_failed;
-    }
-    if (!empty($themes_in_base_failed)) {
-      $issue_details['themes_in_base'] = $themes_in_base_failed;
-    }
-    if (!empty($sites_folder_failed)) {
-      $issue_details[$this->kernel->getSitePath()] = $this->scanSitesFolder();
+    $site_dir = $this->kernel->getSitePath();
+    $scan_dirs = [
+      self::MODULES_BASE,
+      self::THEMES_BASE,
+      $site_dir . '/modules',
+      $site_dir . '/themes',
+    ];
+
+    foreach ($scan_dirs as $dir) {
+      $this->scanFolder($dir);
     }
 
-    if (!empty($issue_details)) {
-      return $this->fail($this->t('There are some issues'), $issue_details);
+    if (!empty($this->issues)) {
+      return $this->fail($this->t('There are some issues'), ['issues' => $this->issues]);
     }
     return $this->success();
   }
 
   /**
-   * Check if project's modules folder doesn't contain modules folders directly.
-   *
-   * @return array
-   *   Modules which are exist in "modules" folder directly.
+   * Check if project's modules folder contain contrib or custom folders.
    */
   protected function scanFolder($path) {
-    $folders = [];
+
+    $this->issues[$path . 'contrib_exists'] = [
+      '@issue_title' => 'Folder contrib does not exist in @path',
+      '@path' => $path,
+    ];
+
+    $this->issues[$path . 'custom_exists'] = [
+      '@issue_title' => 'Folder custom does not exist in @path',
+      '@path' => $path,
+    ];
+
     $modules_dir_list = scandir($path);
 
-    $needle = '.info.yml';
     foreach ($modules_dir_list as $dir) {
-      if (($dir !== '.' && $dir !== '..') && is_dir($path . '/' . $dir)) {
+      if (($dir !== '.' && $dir !== '..') && is_dir(DRUPAL_ROOT . '/' . $path . '/' . $dir)) {
 
         if ($dir === 'contrib') {
-          $folders['contrib_exists'] = TRUE;
+          unset($this->issues[$path . 'contrib_exists']);
+        }
+        if ($dir === 'custom') {
+          unset($this->issues[$path . 'custom_exists']);
         }
 
-        $internal_dirs = scandir($path . '/' . $dir);
-        foreach ($internal_dirs as $item) {
-
-          if (strpos($item, $needle)) {
-            $folders[] = $dir;
-          }
-
-        }
+        $this->scanIssues($path, $dir);
       }
     }
-    return $folders;
   }
 
   /**
-   * Check in site folder in {docroot}/sites/{sitefolder}.
-   *
-   * If folders "modules" or "themes" are exist scan them.
-   *
-   * @return array
-   *   List of fails if they are exist.
+   * Check if project's modules folder doesn't contain modules folders directly.
    */
-  protected function scanSitesFolder() {
-    $folders = [];
-    $sites = $this->kernel->getSitePath();
-    $sites_path = DRUPAL_ROOT . '/' . $sites . '/';
-    $site_dirs = scandir($sites_path);
+  public function scanIssues($path, $dir) {
+    $internal_dirs = scandir(DRUPAL_ROOT . '/' . $path . '/' . $dir);
+    $needle = '.info.yml';
+    foreach ($internal_dirs as $item) {
 
-    foreach ($site_dirs as $dir) {
-      if ($dir === 'modules' || $dir === 'themes') {
-        $sites_modules = $this->scanFolder($sites_path . $dir);
-        $folders[$dir] = $sites_modules;
+      if (strpos($item, $needle)) {
+        $this->issues[$path . $dir] = [
+          '@issue_title' => 'Wrong structure in folder @path (@dir_name)',
+          '@path' => $path,
+          '@dir_name' => $dir,
+        ];
       }
-    }
 
-    return $folders;
-  }
-
-  /**
-   * Get items for auditReportRender if base files structure failed.
-   *
-   * @param array $list
-   *   List of failed modules.
-   * @param string $folder
-   *   Folder in which test was failed.
-   *
-   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
-   *   TranslatableMarkup.
-   */
-  protected function getItems(array $list, $folder): TranslatableMarkup {
-    foreach ($list as $key => $value) {
-      if ($key === 'contrib_exists') {
-        $contrib = TRUE;
-        break;
-      }
     }
-    $placeholder = ['%folder' => $folder];
-    return $contrib ?
-      $this->t('Base "%folder" folder contains %folder (contrib folder exists):', $placeholder) :
-      $this->t("Base \"%folder\" folder contains %folder (contrib folder doesn't exists):", $placeholder);
-  }
-
-  /**
-   * Get item if files structure failed in sites folder for multisite.
-   *
-   * @param array $list
-   *   List of failed sites.
-   *
-   * @return array
-   *   List of failed sites.
-   */
-  protected function getItemsMultiSitesFailed(array $list) {
-    foreach ($list as $key => $value) {
-      $sites_list[] = $key;
-    }
-    return [
-      '#theme' => 'item_list',
-      '#title' => $this->t('Multisite folders:'),
-      '#list_type' => 'ul',
-      '#items' => $sites_list,
-    ];
   }
 
 }
