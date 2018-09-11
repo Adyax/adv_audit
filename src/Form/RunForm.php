21a366d2fb6db39f5cc59dee1240c68f23b9cd6b
Ivan Tsekhmistro <itsekhmistro@adyax.com>
2018-09-11 15:42:04 +0300

    Skip disabled Categories in RunForm.

--- src/Form/RunForm.php
+++ src/Form/RunForm.php
@@ -62,7 +62,7 @@ class RunForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['process_list'] = [
       '#type' => 'fieldset',
-      '#title' => $this->t('Available test list:'),
+      '#title' => $this->t('Available Audits:'),
       'list' => ['#markup' => $this->buildProcessItems()],
     ];
 
@@ -82,14 +82,19 @@ class RunForm extends FormBase {
     // Get all available tests.
     $categories = $this->configCategories->get('adv_audit_settings')['categories'];
     foreach ($this->auditTestManager->getPluginsByCategory() as $category_id => $plugins) {
+      if (empty($categories[$category_id]['status'])) {
+        // Skip disabled categories.
+        continue;
+      }
+
       $items[$category_id]['title'] = $categories[$category_id]['label'];
       $items[$category_id]['items'] = [];
       foreach ($plugins as $plugin_id => $plugin_definition) {
-        $plugin_instance = $this->auditTestManager->createInstance($plugin_id);
-        if (!$plugin_instance->getStatus()) {
+        if (!$this->isPluginEnabled($plugin_id)) {
           continue;
         }
-        $items[$category_id]['items'][$plugin_id]['label'] = $plugin_definition['label']->__toString();
+
+        $items[$category_id]['items'][$plugin_id]['label'] = $plugin_definition['label'];
         $items[$category_id]['items'][$plugin_id]['id'] = $plugin_definition['id'];
       }
     }
@@ -104,15 +109,26 @@ class RunForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $categories = $this->configCategories->get('adv_audit_settings')['categories'];
     // Run AuditChecks implemented via plugins.
-    $tests = $this->auditTestManager->getDefinitions();
-    foreach ($tests as $key => $test) {
-      $plugin_instance = $this->auditTestManager->createInstance($test['id']);
-      // Remove disabled plugins from audit.
-      if (!$plugin_instance->getStatus()) {
-        unset($tests[$key]);
+    $all_audit_plugins = $this->auditTestManager->getDefinitions();
+    $audit_plugins_to_run = [];
+
+    foreach ($all_audit_plugins as $plugin_id => $plugin) {
+      $category = $plugin['category'];
+      if (empty($categories[$category]['status'])) {
+        // Skip plugins from disabled categories.
+        continue;
       }
+
+      if (!$this->isPluginEnabled($plugin_id)) {
+        continue;
+      }
+
+      // Add the plugin to Batch jobs.
+      $audit_plugins_to_run[$plugin_id] = $plugin;
     }
+
     $batch = [
       'title' => $this->t('Running process audit'),
       'init_message' => $this->t('Prepare to process.'),
@@ -121,7 +137,7 @@ class RunForm extends FormBase {
       'operations' => [
         [
           [AuditRunBatch::class, 'run'],
-          [array_keys($tests), []],
+          [array_keys($audit_plugins_to_run), []],
         ],
       ],
       'finished' => [
@@ -129,6 +145,31 @@ class RunForm extends FormBase {
       ],
     ];
     batch_set($batch);
+  }
+
+  /**
+   * Check if the Audit plugin is enabled.
+   *
+   * @param string $plugin_id
+   *   Audit plugin.
+   *
+   * @return bool
+   *   TRUE if the plugin is enabled.
+   */
+  protected function isPluginEnabled($plugin_id) {
+    try {
+      $plugin_instance = $this->auditTestManager->createInstance($plugin_id);
+      // Skip disabled plugins from audit.
+      if (!$plugin_instance->getStatus()) {
+        return FALSE;
+      }
+    }
+    catch (\Exception $e) {
+      // Nothing to do here.
+      // Broken plugins are still considered to be enabled.
+    }
+
+    return TRUE;
   }
 
 }
