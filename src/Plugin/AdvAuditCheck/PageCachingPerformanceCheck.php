6fd810f01551eb81c729271530e92fee8e8d9fa3
YDrapoy <ydrapoy@adyax.com>
2018-09-12 19:24:43 +0300

    issues support for PageCachingPerformanceCheck

--- src/Plugin/AdvAuditCheck/PageCachingPerformanceCheck.php
+++ src/Plugin/AdvAuditCheck/PageCachingPerformanceCheck.php
@@ -101,10 +101,16 @@ class PageCachingPerformanceCheck extends AdvAuditCheckBase implements Container
     if (((!empty($response_header) && strpos($response_header, 'HIT') !== FALSE)
       || (!empty($varnish_hits) && $varnish_hits > 0))
       && $cache_lifetime['max_age'] > 0) {
-      return new AuditReason($this->id(), AuditResultResponseInterface::RESULT_PASS);
+      return $this->success();
     }
 
-    return new AuditReason($this->id(), AuditResultResponseInterface::RESULT_FAIL);
+    return $this->fail(NULL, [
+      'issues' => [
+        'page_caching_performance' => [
+          '@issue_title' => 'Need to check and fix cache settings.'
+        ],
+      ],
+    ]);
   }
 
 }
