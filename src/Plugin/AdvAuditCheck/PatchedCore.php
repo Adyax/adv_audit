e47c80aa0aec03bd94e10855b8dd27f3c819e539
Alex <ashipilov@adyax.com>
2018-09-13 17:54:42 +0300

    #304964 - patched core plugin

---
+++ src/Plugin/AdvAuditCheck/PatchedCore.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\adv_audit\Plugin\AdvAuditCheck;
+
+use Drupal\hacked\Controller\HackedController;
+
+/**
+ * Check Drupal Core for patches.
+ *
+ * @AdvAuditCheck(
+ *   id = "patched_core",
+ *   label = @Translation("Patched Drupal core."),
+ *   category = "core_and_modules",
+ *   requirements = {
+ *     "module": {
+ *      "hacked:2.0-beta",
+ *     },
+ *   },
+ *   enabled = true,
+ *   severity = "high"
+ * )
+ */
+class PatchedCore extends PatchedModulesCheck {
+
+  /**
+   * Process checkpoint review.
+   */
+  public function perform() {
+    $hacked = new HackedController();
+    $hacked = $hacked->hackedStatus();
+
+    $issues = [];
+    foreach ($hacked['#data'] as $project) {
+      if ($project['counts']['different'] != 0 && $project['project_type'] == 'core') {
+        $issues['hacked_core'] = [
+          '@issue_title' => '@title was hacked (@count changes)',
+          '@title' => $project['title'],
+          '@count' => $project['counts']['different'],
+        ];
+      }
+    }
+
+    if (!empty($issues)) {
+      return $this->fail('', ['issues' => $issues]);
+    }
+
+    return $this->success();
+  }
+
+}
