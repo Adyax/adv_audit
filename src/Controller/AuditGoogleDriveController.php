f06eae9de1dfe1558522e57ffd3ba5d8ea82d40f
Alex <ashipilov@adyax.com>
2018-09-25 19:59:34 +0300

    #325695 - auth to google drive

--- src/Controller/AuditGoogleDriveController.php
+++ src/Controller/AuditGoogleDriveController.php
@@ -2,9 +2,12 @@
 
 namespace Drupal\adv_audit\Controller;
 
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\Core\Url;
 use Google_Service_Drive;
 use Mpdf\Mpdf;
 use Mpdf\Output\Destination;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Google_Client;
 use Google_Service_Drive_DriveFile;
@@ -35,24 +38,32 @@ class AuditGoogleDriveController {
     ];
 
     $client = new Google_Client();
-    $client->setClientId('337518780648-e2t289mbufr3m81oe0v7caa1qgdg0pt9.apps.googleusercontent.com');
-    $client->setClientSecret('m1PZSJJ9NyEKM9DJdc5AlL4p');
-    $client->authorize();
+    $client->setClientId('337518780648-m9gli5j24r844gs1b6ukuiphn913ec8s.apps.googleusercontent.com');
+    $client->setClientSecret('VmhjkFuPq7dDJ1pM4Yf0o7sj');
+    $client->setRedirectUri('http://localhost:8080');
+    $client->setScopes(array('https://www.googleapis.com/auth/drive'));
     $driveService = new Google_Service_Drive($client);
+    if (!isset($_REQUEST['code'])) {
+      $authUrl = $client->createAuthUrl();
+      $redirect = new TrustedRedirectResponse($authUrl);
+      $redirect->send();
+    }
+
+    $accessToken = $client->authenticate($_REQUEST['code']);
+    $client->setAccessToken($accessToken);
+    // take maximum number of elements
+    $list = $driveService->files->listFiles()->getFiles();
+    $num = count($list);
     $fileMetadata = new Google_Service_Drive_DriveFile(array(
-      'name' => 'Auditor Report ',
+      'name' => 'Auditor Report - ' . $date_report,
       'mimeType' => 'application/vnd.google-apps.document'));
     $content = $renderer;
     $file = $driveService->files->create($fileMetadata, array(
-      'data' => $content,
-      'mimeType' => 'application/html',
-      'uploadType' => 'multipart',
       'fields' => 'id'));
-    printf("File ID: %s\n", $file->id);
 
-    return new Response($renderer, 200, $headers);
+    $ret = 'All files: <strong>'.$num.'</strong><br />';
+    $ret .= 'File id = ' . $file->id;
+    return new Response($ret, 200, $headers);
   }
-
-
 
 }
