a172c04945e9a47936c58fccf68b71e5bb184055
Aliaksei Lyzo <alyzo@adyax.com>
2018-11-12 17:24:59 +0300

    Chnege sort order on report list

--- src/Entity/AuditEntityListBuilder.php
+++ src/Entity/AuditEntityListBuilder.php
@@ -43,4 +43,18 @@ class AuditEntityListBuilder extends EntityListBuilder {
     return $row + parent::buildRow($entity);
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEntityIds() {
+    $query = $this->getStorage()->getQuery()
+      ->sort($this->entityType->getKey('id'), 'DESC');
+
+    // Only add the pager if a limit is specified.
+    if ($this->limit) {
+      $query->pager($this->limit);
+    }
+    return $query->execute();
+  }
+
 }
